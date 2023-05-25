c987076cd2b5b49ebb7f808ffe2370944407858f
Faisal ahmed <faisal2410@yahoo.com>
2023-05-25 22:52:06 +0600

    updated

---
+++ app/Http/Controllers/Api/V1/CompleteTaskController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\TaskResource;
+use App\Models\Task;
+use Illuminate\Http\Request;
+
+class CompleteTaskController extends Controller {
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke( Request $request, Task $task ) {
+        $task->is_completed = $request->is_completed;
+
+        $task->save();
+
+        return TaskResource::make( $task );
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\V1\TaskController;
+use App\Http\Controllers\Api\V1\CompleteTaskController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,7 +19,8 @@ use App\Http\Controllers\Api\V1\TaskController;
 Route::prefix( 'v1' )->group( function () {
 
     Route::apiResource( "/tasks", TaskController::class );
-    
+     Route::patch("/tasks/{task}/complete",CompleteTaskController::class);
+
 
 } );
 
